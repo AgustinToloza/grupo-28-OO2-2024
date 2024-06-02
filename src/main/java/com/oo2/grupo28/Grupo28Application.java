3cd61694cc459b4ba4ec1908ba10ff1c3f3b9883
Agustín Toloza <agustintoloza757@gmail.com>
2024-06-02 10:50:22 -0300

    Se agrega el Proyecto

---
+++ src/main/java/com/oo2/grupo28/Grupo28Application.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo28Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oo2/grupo28/Grupo28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
