c16ed3042eb531f4479487e0a69f552c75006bfd
Agustín Toloza <agustintoloza757@gmail.com>
2024-06-17 16:47:59 -0300

    Login al Sistema segun ROL

--- src/main/java/com/oo2/grupo28/controllers/HomeController.java
+++ src/main/java/com/oo2/grupo28/controllers/HomeController.java
@@ -1,8 +1,15 @@
 package com.oo2.grupo28.controllers;
 
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +18,46 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.oo2.grupo28.helpers.ViewRouteHelper;
+import com.oo2.grupo28.services.IProductoService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
-	
+		
+		private IProductoService productoService;
+		
+		public HomeController(IProductoService productoService) {
+			super();
+			this.productoService = productoService;
+		}
+
 		//GET Example: SERVER/index
 		@GetMapping("/index")
 		public ModelAndView index() {
-			ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.INDEX);
+			
 			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-			modelAndView.addObject("username", user.getUsername());
+			Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+			ModelAndView modelAndView = new ModelAndView();
+			
+			for (GrantedAuthority authority : authorities) {
+	            if (authority.getAuthority().equals("ROLE_ADMIN")) {
+	            	
+	            	modelAndView.setViewName(ViewRouteHelper.ADMIN_INDEX);
+	    			modelAndView.addObject("username", user.getUsername());
+	    			
+	            } else if (authority.getAuthority().equals("ROLE_USER")) {
+	            	
+	            	modelAndView.setViewName(ViewRouteHelper.USER_INDEX);
+	    			modelAndView.addObject("username", user.getUsername());
+	    			modelAndView.addObject("productos", productoService.getAll());
+	            }
+	        }
+			
 			return modelAndView;
+			
 		}
 
-		//GET Example: SERVER/hello?name=someName
+		/*GET Example: SERVER/hello?name=someName
 		@GetMapping("/hello")
 		public ModelAndView helloParams1(@RequestParam(name="name", required=false, defaultValue="null") String name) {
 			ModelAndView mV = new ModelAndView(ViewRouteHelper.HELLO);
@@ -39,7 +71,7 @@ public class HomeController {
 			ModelAndView mV = new ModelAndView(ViewRouteHelper.HELLO);
 			mV.addObject("name", name);
 			return mV;
-		}
+		}*/
 
 		@GetMapping("/")
 		public RedirectView redirectToHomeIndex() {
