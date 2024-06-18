62dfc72506e68a5b6d0ab3c9c04787e13ede74ec
Agustín Toloza <agustintoloza757@gmail.com>
2024-06-18 14:58:27 -0300

    Actualizacion Capa Reportes: Informe de Stocks

--- src/main/java/com/oo2/grupo28/controllers/ReporteController.java
+++ src/main/java/com/oo2/grupo28/controllers/ReporteController.java
@@ -1,6 +1,8 @@
 package com.oo2.grupo28.controllers;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.oo2.grupo28.entities.Stock;
 import com.oo2.grupo28.helpers.ViewRouteHelper;
 import com.oo2.grupo28.services.ILoteService;
+import com.oo2.grupo28.services.IStockService;
 
 @Controller
 @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -18,11 +22,13 @@ import com.oo2.grupo28.services.ILoteService;
 public class ReporteController {
 	
 	private ILoteService loteService;
+	private IStockService stockService;
 	
 
-	public ReporteController(ILoteService loteService) {
+	public ReporteController(ILoteService loteService, IStockService stockService) {
 		super();
 		this.loteService = loteService;
+		this.stockService = stockService;
 	}
 	
 	@GetMapping("/")
@@ -42,6 +48,18 @@ public class ReporteController {
 	public ModelAndView loteFecha(@PathVariable("fecha") LocalDate fecha) throws Exception {
 		ModelAndView mAV = new ModelAndView(ViewRouteHelper.REPORTE_LOTE);
 		mAV.addObject("lotes", loteService.findByFecha(fecha));
+		return mAV;
+	}
+	
+	@GetMapping("/stock")
+	public ModelAndView informeStock() {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.REPORTE_STOCK);
+		
+		List<Stock> stocksOrdenados = stockService.getAll().stream()
+				.sorted((s1, s2) -> Integer.compare(s2.getCantidadActual(), s1.getCantidadActual()))
+				.collect(Collectors.toList());
+		
+		mAV.addObject("stocks",stocksOrdenados);
 		return mAV;
 	}
 	
