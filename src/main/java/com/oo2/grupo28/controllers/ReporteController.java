39b9259e30a84bc6c0b84d75cf0544b025bf9dbd
Agustín Toloza <agustintoloza757@gmail.com>
2024-06-18 15:08:31 -0300

    Actualizacion Capa Reportes e Informe de Stocks

--- src/main/java/com/oo2/grupo28/controllers/ReporteController.java
+++ src/main/java/com/oo2/grupo28/controllers/ReporteController.java
@@ -24,7 +24,6 @@ public class ReporteController {
 	private ILoteService loteService;
 	private IStockService stockService;
 	
-
 	public ReporteController(ILoteService loteService, IStockService stockService) {
 		super();
 		this.loteService = loteService;
