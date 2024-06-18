9e2b938a9a577a39ef94ed5af175a8da731d968c
Lucaledesma <lucaledesma55@gmail.com>
2024-06-18 14:19:11 -0300

    Actualizacion capa Reportes y se agrega informe de Lotes

--- src/main/java/com/oo2/grupo28/dtos/LoteDTO.java
+++ src/main/java/com/oo2/grupo28/dtos/LoteDTO.java
@@ -2,6 +2,9 @@ package com.oo2.grupo28.dtos;
 
 import java.time.LocalDate;
 
+import com.oo2.grupo28.entities.Pedido;
+import com.oo2.grupo28.entities.Producto;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,12 +17,18 @@ public class LoteDTO {
 	private LocalDate fechaRecepcion;
 
 	private int cantidadRecibida;
+	
+	private Producto producto;
+	
+	private Pedido pedido;
 
-	public LoteDTO(int id, LocalDate fechaRecepcion, int cantidadRecibida) {
+	public LoteDTO(int id, LocalDate fechaRecepcion, int cantidadRecibida, Producto producto, Pedido pedido) {
 		super();
 		this.setId(id);
 		this.fechaRecepcion = fechaRecepcion;
 		this.cantidadRecibida = cantidadRecibida;
+		this.producto = producto;
+		this.pedido = pedido;
 	}
 	
 }
